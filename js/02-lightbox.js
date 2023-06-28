1f8ade93b6e87eecfe2fb05295f4e0fa96394b53
BerkovskiiAlex <berkovskii845@gmail.com>
2023-06-28 12:39:40 +0300

    create 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+/** @format */
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulInsert = document.querySelector(".gallery");
+
+function addGallery(galleryItems) {
+  const result = galleryItems
+    .map((image) => {
+      return `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="large-image.jpg" alt="${image.description}"></a></li>`;
+    })
+    .join("");
+  ulInsert.insertAdjacentHTML("beforeend", result);
+}
+
+addGallery(galleryItems);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionDelay: 250,
+});
